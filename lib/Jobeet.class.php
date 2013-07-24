63e4870b407b2d75f226857e6328534a6b975eab
Enrique Barbeito García <ebarbeito@gmail.com>
2013-07-24 11:53:32 +0200

    added chapter 8

--- lib/Jobeet.class.php
+++ lib/Jobeet.class.php
@@ -9,9 +9,28 @@ class Jobeet
 {
 	static public function slugify($text)
 	{
-		// replace all non letters or digits by -
-		$text = preg_replace('/\W+/', '-', $text);
-		$text = strtolower(trim($text, '-'));
+		// replace non letter or digits by -
+		$text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+
+		// trim
+		$text = trim($text, '-');
+
+		// transliterate
+		if (function_exists('iconv')) {
+		  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+		}
+
+		// lowercase
+		$text = strtolower($text);
+
+		// remove unwanted characters
+		$text = preg_replace('#[^-\w]+#', '', $text);
+
+		// default value for the empty string
+		if (!strlen($text)) {
+		  return 'n-a';
+		}
+
 		return $text;
 	}
 }
---
+++ test/bootstrap/doctrine.php
@@ -0,0 +1,7 @@
+<?php
+
+include(dirname(__FILE__) . '/unit.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('public', 'test', true);
+new sfDatabaseManager($configuration);
+Doctrine_Core::loadData(sfConfig::get('sf_test_dir') . '/fixtures');
